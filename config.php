479649990ccfd787d7e0ebe72635e946b8c6f4b0
CyberRad <cbrown@radnetworx.com>
2013-05-07 10:40:55 -0400

    Adding sanitized config file.

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php 
+	//Site Name
+	$title = 'SMHack Ticketing System';
+	
+	//Members Database
+	$sqlServertype = 'mysql'; //dblib, mysql, odbc, pgsql, sqlite, sqlite2
+	$sqlServer = '';
+	$sqlDBname = '';
+	$sqlUsername = '';
+	$sqlPassword = '';
+	
+	//Ticket Database
+	$sqlTicketservertype = 'mysql'; //dblib, mysql, odbc, pgsql, sqlite, sqlite2
+	$sqlTicketserver = '';
+	$sqlTicketdbname = '';
+	$sqlTicketusername = '';
+	$sqlTicketpassword = '';
+?>
